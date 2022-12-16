1e3dc130f2a97eb07aee89b863afaff0a5fee372
Your NameNazar <nazarkruk@icloud.com>
2022-12-15 21:30:27 -0500

    Adding Favorites

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,11 +1,11 @@
-import { ScrollView, Text, View } from "react-native";
-import { Card, ListItem, Avatar } from "react-native-elements";
-import { useState } from "react";
-import { PARTNERS } from "../shared/partners";
+import { ScrollView, Text } from "react-native";
+import { Avatar, Card, ListItem } from "react-native-elements";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
 
-const Mission = () => {
+function Mission() {
   return (
-    <Card wrapperStyle={{ margin: 20 }}>
+    <Card>
       <Card.Title>Our Mission</Card.Title>
       <Card.Divider />
       <Text style={{ margin: 10 }}>
@@ -19,27 +19,26 @@ const Mission = () => {
       </Text>
     </Card>
   );
-};
+}
 
 const AboutScreen = () => {
-  const [partners, setPartners] = useState(PARTNERS);
+  const partners = useSelector((state) => state.partners);
+
   return (
     <ScrollView>
       <Mission />
-      <Card wrapperStyle={{ margin: 20 }}>
+      <Card>
         <Card.Title>Community Partners</Card.Title>
         <Card.Divider />
-        {partners.map((partner) => {
-          return (
-            <ListItem key={partner.id}>
-              <Avatar source={partner.image} rounded />
-              <ListItem.Content>
-                <ListItem.Title>{partner.name}</ListItem.Title>
-                <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
-              </ListItem.Content>
-            </ListItem>
-          );
-        })}
+        {partners.partnersArray.map((partner) => (
+          <ListItem key={partner.id}>
+            <Avatar rounded source={{ uri: baseUrl + partner.image }} />
+            <ListItem.Content>
+              <ListItem.Title>{partner.name}</ListItem.Title>
+              <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        ))}
       </Card>
     </ScrollView>
   );
