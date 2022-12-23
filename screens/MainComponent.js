15d40102ecde41a8f864f7c392cd3405ddef243a
Your NameNazar <nazarkruk@icloud.com>
2022-12-22 21:47:52 -0500

    Adding Favorites

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -20,6 +20,7 @@ import { fetchPartners } from "../features/partners/partnersSlice";
 import { fetchCampsites } from "../features/campsites/campsitesSlice";
 import { fetchPromotions } from "../features/promotions/promotionsSlice";
 import { fetchComments } from "../features/comments/commentsSlice";
+import FavoritesScreen from "./FavoritesScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -119,6 +120,29 @@ const ReservationNavigator = () => {
   );
 };
 
+const FavoritesNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={({ navigation }) => ({
+          title: "Favorite Campsites",
+          headerLeft: () => (
+            <Icon
+              name="heart"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
   return (
@@ -199,7 +223,6 @@ const Main = () => {
                 color={color}
               />
             ),
-            headerShown: false,
           }}
         />
         <Drawer.Screen
@@ -216,7 +239,6 @@ const Main = () => {
                 color={color}
               />
             ),
-            headerShown: false,
           }}
         />
         <Drawer.Screen
@@ -233,7 +255,22 @@ const Main = () => {
                 color={color}
               />
             ),
-            headerShown: false,
+          }}
+        />
+        <Drawer.Screen
+          name="Favorites"
+          component={FavoritesNavigator}
+          options={{
+            title: "My Favorites",
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="heart"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
           }}
         />
         <Drawer.Screen
@@ -250,7 +287,6 @@ const Main = () => {
                 color={color}
               />
             ),
-            headerShown: false,
           }}
         />
         <Drawer.Screen
@@ -267,7 +303,6 @@ const Main = () => {
                 color={color}
               />
             ),
-            headerShown: false,
           }}
         />
       </Drawer.Navigator>
