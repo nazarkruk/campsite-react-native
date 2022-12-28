5722231e76ff0577ff5c4a1be3226f73d45a62b4
Your NameNazar <nazarkruk@icloud.com>
2022-12-27 23:40:45 -0500

    Adding Sharing Functionality

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -5,7 +5,7 @@ import {
   View,
   PanResponder,
   Alert,
-  Modal,
+  Share,
 } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
@@ -56,6 +56,19 @@ const RenderCampsite = (props) => {
     },
   });
 
+  const shareCampsite = (title, message, url) => {
+    Share.share(
+      {
+        title,
+        message: `${title}: ${message} ${url}`,
+        url,
+      },
+      {
+        dialogTitle: "Share " + title,
+      }
+    );
+  };
+
   if (campsite) {
     return (
       <Animatable.View
@@ -92,6 +105,20 @@ const RenderCampsite = (props) => {
               raised
               reverse
               onPress={props.onShowModal}
+            />
+            <Icon
+              name="share"
+              type="font-awesome"
+              color="#5637DD"
+              raised
+              reverse
+              onPress={() =>
+                shareCampsite(
+                  campsite.name,
+                  campsite.description,
+                  baseUrl + campsite.image
+                )
+              }
             />
           </View>
         </Card>
